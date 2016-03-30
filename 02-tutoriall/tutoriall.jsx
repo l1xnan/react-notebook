ab59d8d6a5591ece6c1553563702c0dbd40f6ad5
l1xnan <lixnan.g@gmail.com>
2016-03-30 21:15:32 +0800

    test

---
+++ 02-tutoriall/tutoriall.jsx
@@ -0,0 +1,14 @@
+let CommentBox = React.createClass({
+    render: function(){
+        return (
+            <div className="commentBox">
+                Hello, World! I am a CommentBox.
+            </div>
+        );
+    }
+});
+
+React.render(
+    <CommentBox />,
+    document.getElementById('content')
+);
